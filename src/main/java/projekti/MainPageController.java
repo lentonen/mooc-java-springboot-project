4c28db031aa8587cd51bb83d967335f0753b7700
Leinonen Henri Matias <henri.m.leinonen@student.jyu.fi>
2021-11-19 14:50:13 +0200

    kirjautuminen onnistuu

---
+++ src/main/java/projekti/MainPageController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekti;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author Henri
+ */
+@Controller
+public class AccountController {
+    
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @GetMapping("/accounts")
+    public String list(Model model) {
+        model.addAttribute("accounts", accountRepository.findAll());
+        return "accounts";
+    }
+
+    @PostMapping("/accounts")
+    public String add(@RequestParam String username, @RequestParam String realname, @RequestParam String password, @RequestParam String urlAddress) {
+        if (accountRepository.findByUsername(username) != null) {
+            return "redirect:/accounts";
+        }
+        
+        Account a = new Account(username, realname, passwordEncoder.encode(password), urlAddress, new ArrayList<Account>());
+        accountRepository.save(a);
+        return "redirect:/accounts";
+    }
+    
+}
