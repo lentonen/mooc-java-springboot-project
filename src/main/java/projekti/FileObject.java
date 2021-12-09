7cbe278cd6666100fa89a3e5be1e101f1356107c
Leinonen Henri Matias <henri.m.leinonen@student.jyu.fi>
2021-12-09 14:40:57 +0200

    fo kuvien annotaatiota korjattu

--- src/main/java/projekti/FileObject.java
+++ src/main/java/projekti/FileObject.java
@@ -6,6 +6,7 @@ import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Data
 public class FileObject extends AbstractPersistable<Long> {
     @Lob
+    @Type(type = "org.hibernate.type.ImageType")
     private byte[] content;
     
     @ManyToOne
